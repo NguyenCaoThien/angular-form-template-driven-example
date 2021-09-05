2ca71f63e2d1a1db2534b808f47064be5bd6511c
tua <thien.nguyen@elca.vn>
2021-09-05 15:43:16 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild("f") signUpForm: NgForm;
+  user={
+    username: '',
+    email: '',
+    subscription: '',
+    password: '',
+    gender: ''
+  }
+
   title = 'form-exercise';
+  genders = ["male", "female"];
+  userReply ='';
+  isSubmitted = false;
+
+  suggestName(): void{
+    this.signUpForm.form.patchValue({
+      userData:{
+        username: "test",
+        subscription: "advance"
+      }
+    })
+  }
+  
+  onSubmit(): void{
+    console.log(this.signUpForm)
+    this.isSubmitted = true;
+    this.user.username = this.signUpForm.form.value.userData.username;
+    this.user.email = this.signUpForm.form.value.userData.email;
+    this.user.subscription = this.signUpForm.form.value.userData.subscription;
+    this.user.password = this.signUpForm.form.value.userData.password;
+    this.user.gender = this.signUpForm.form.value.userData.gender;
+  }
 }
